0c7bfa8d054b6b5730433e5ffbeab81d97997580
David Gomes <david.gomes@tuta.io>
2023-05-31 20:55:02 -0300

    Update ring.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from ring import Ring, Message
+from ring import Ring, Message, MessageType
 
 
 def main():
@@ -8,16 +8,20 @@ def main():
 
     send_port = 6000
     recv_port = 6001
+    machine_id = 2
 
     if sys.argv[1] == 's':
+        machine_id = 1
         send_port = 6001
         recv_port = 6000
 
-    ring = Ring(send_port, recv_port, '127.0.0.1', '127.0.0.1')
+    ring = None
+
+    ring = Ring(machine_id, send_port, recv_port, '127.0.0.1', '127.0.0.1')
     ring.setup()
 
     if sys.argv[1] == 's':
-        ring.send_message(Message('this is a move'))
+        ring.send_message(Message(True, MessageType.PLAY_CARDS, 'this is a move'))
     else:
         ring.recv_message()
 
--- ring.py
+++ ring.py
@@ -1,17 +1,34 @@
 import socket
 import pickle
 
+from enum import Enum
+
 
 START_MARKER = 0b01110101
 END_MARKER = 0b0111010101
 
 
+class MessageType(Enum):
+    PLAY_CARDS = 1
+    PASS = 2
+    TOKEN = 3
+    # etc. 
+
+
 class Message():
-    def __init__(self, move: str):
+    def __init__(self, origin: bool, type, move: str):
         self.start_marker = START_MARKER
-        self.origin = 1
+        self.origin = 1 if bool else 0
+
+        self.type = type
+
+        if isinstance(type, MessageType):
+            self.type = type.value
+
         self.move = move
+
         self.recv_confirm = 0
+
         self.end_marker = END_MARKER
         pass
 
@@ -19,6 +36,7 @@ class Message():
         return (f'--- Message: \n'
             f'\tstart_marker: {self.start_marker}\n'
             f'\torigin: {self.origin}\n'
+            f'\ttype: {self.type}\n'
             f'\tmove: {self.move}\n'
             f'\treceipt_confirmation: {self.recv_confirm}\n'
             f'\tend_marker: {self.end_marker}\n'
@@ -29,6 +47,7 @@ class Message():
 
         buffer.append(self.start_marker)
         buffer.append(self.origin)
+        buffer.append(self.type)
         buffer.append(self.move)
         buffer.append(self.recv_confirm)
         buffer.append(self.end_marker)
@@ -37,10 +56,13 @@ class Message():
 
     @staticmethod
     def buffer_to_message(buffer):
-        message = Message(None)
+        message = Message(buffer[1], buffer[2], buffer[3])
 
-        #TODO
-        return
+        message.start_marker = buffer[0]
+        message.recv_confirm = buffer[4]
+        message.end_marker = buffer[5]
+
+        return message
 
     pass
 
@@ -48,11 +70,14 @@ class Message():
 class Ring():
     def __init__(
         self,
+        machine_id: int,
         send_port: int,
         recv_port: int,
         send_address,
         recv_address
     ):
+        self.machine_id = machine_id
+
         self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -61,6 +86,11 @@ class Ring():
 
         self.send_address = send_address
         self.recv_address = recv_address
+
+        self.has_token = False
+
+        if machine_id == 1:
+            self.has_token = True
 
         pass
 
@@ -92,11 +122,19 @@ class Ring():
     def recv_message(self) -> Message:
         print(f'Receiving...')
 
-        data = self.recv_socket.recv(self.recv_port)
-        buffer = pickle.loads(data)
-        print(f'Data: {buffer}')
+        start_marker = None
+        
+        while (start_marker != START_MARKER):
+            data = self.recv_socket.recv(self.recv_port)
+            buffer = pickle.loads(data)
+            start_marker = buffer[0]
 
-        return Message(None)
+        print(f'Data: {buffer}')
+        
+        message = Message.buffer_to_message(buffer)
+        print(f'Message: {message}')
+
+        return message
 
     pass
 
