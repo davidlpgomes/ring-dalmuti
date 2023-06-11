7377afca3f61dac439d32c4b2afdacf764f3ebe8
David Gomes <david.gomes@tuta.io>
2023-06-10 21:14:40 -0300

    Add logging to ring.py

--- ring.py
+++ ring.py
@@ -1,3 +1,4 @@
+import logging
 import socket
 import pickle
 
@@ -118,9 +119,10 @@ class Ring():
 
     def setup(self):
         self.recv_socket.bind((self.recv_address, self.recv_port))
-        print(f'[S-RECV] Binded to {self.recv_address} on {self.recv_port}')
+        logging.debug(f'Binded to {self.recv_address} on {self.recv_port}')
 
         self.recv_socket.settimeout(SOCKET_TIMEOUT)
+        logging.debug(f'Set recv socket timeout to {SOCKET_TIMEOUT}')
 
         return
 
@@ -135,24 +137,30 @@ class Ring():
         return
 
     def send_message(self, message: Message):
-        #print(message.send_repr())
-        #input('send_message')
+        logging.debug(f'[SEND] Sending {message}')
+
         data = pickle.dumps(message.get_buffer())
         recv_message = None
 
         while True:
             self.send(data)
+
+            logging.debug('[SEND] Waiting message')
             recv_message = self.recv_message()
+            logging.debug(f'[SEND] Received message {recv_message}')
 
             if recv_message.origin == self.machine_id:
+                logging.debug(f'[SEND] Message went through the entire ring')
                 recv_message = self.set_received(recv_message)
                 break
 
-            return
+            logging.warning(f'[SEND] Did not received the sent message')
+
+        return
 
     def send_message_to_next(self, message: Message):
-        #print(message.send_repr())
-        #input('send_message_to_next')
+        logging.debug(f'[SEND-N] Sending to next machine {message}')
+
         data = pickle.dumps(message.get_buffer())
         self.send(data)
 
@@ -174,36 +182,51 @@ class Ring():
             start_marker = buffer[0]
 
         message = Message.buffer_to_message(buffer)
-        # print(message)
+        logging.debug(f'[RECV] Received message: {message}')
 
         if message.type == MessageType.TOKEN.value:
-            if int(message.move) == self.machine_id:
+            id = int(message.move)
+
+            logging.debug(f'[RECV] Message is TOKEN for machine {id}')
+
+            if id == self.machine_id:
+                logging.debug('[RECV] Received TOKEN')
                 self.has_token = True
             else:
+                logging.debug('[RECV] Giving TOKEN')
                 self.give_token(int(message.move))
 
         return message
     
     def wait_token_settle(self):
-        while 1:
+        logging.debug('[WTS] Waiting token settle')
+
+        while True:
             message = self.recv_and_send_message()
+
             if self.has_token:
+                logging.debug('[WTS] Received token, senting TOKEN_SETTLED')
                 self.send_message(Message(self.machine_id, MessageType.TOKEN_SETTLED, ''))
                 break
             elif message.type == MessageType.TOKEN_SETTLED.value:
+                logging.debug('[WTS] Received TOKEN_SETTLED')
                 break
-        print('token settled')
+
         return
 
 
     def recv_and_send_message(self) -> Message:
+        logging.debug('[RECV&SEND] Receiving and sending message')
+
         message = self.recv_message()
         message = self.set_received(message)
 
         if message.type == MessageType.TOKEN.value:
+            logging.debug('[RECV&SEND] Message is TOKEN, returning')
             return message
 
         if not self.has_token:
+            logging.debug('[RECV&SEND] Sending message to next')
             self.send_message_to_next(message)
 
         return message
@@ -212,26 +235,30 @@ class Ring():
         if machine_id == -1:
             machine_id = self.machine_id % self.num_machines + 1
 
+        logging.debug(f'[GT] Giving TOKEN to {machine_id}')
+
         if machine_id == self.machine_id:
             message = Message(self.machine_id, MessageType.TOKEN_SETTLED, '')
-            #print(message.send_repr())
-            #input('token_settled')
             data = pickle.dumps(message.get_buffer())
             self.send(data)
+
+            logging.debug(f'[GT] Giving TOKEN to yourself, TOKEN_SETTLED sent')
             return
 
         message = Message(self.machine_id, MessageType.TOKEN, str(machine_id))
-        #print(message.send_repr())
-        #input('give_token')
         data = pickle.dumps(message.get_buffer())
 
         self.send(data)
         self.has_token = False
 
+        logging.debug(f'[GT] TOKEN sent')
+
         return
 
     def set_received(self, message: Message):
         message.recv_confirm |= 2 ** (self.num_machines - self.machine_id)
+        logging.debug('[SET-R] Set message received bit')
+
         return message
 
     pass
